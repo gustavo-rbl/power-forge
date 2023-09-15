ca4bf4867311fc3220edd8ec56d6d92a4ad4f862
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-09-14 20:36:04 -0500

    add scroll reset on location change

--- src/components/Layout/RootLayout.tsx
+++ src/components/Layout/RootLayout.tsx
@@ -1,8 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { useEffect, useRef } from "react";
 
 function RootLayout() {
+  const location = useLocation();
+  const locationRef = useRef(location.key);
+
+  useEffect(() => {
+    let isMounted = true;
+    const sameLocation = locationRef.current === location.key;
+
+    if (!sameLocation) window.scroll(0, 0);
+
+    return () => {
+      isMounted = false;
+    };
+  }, [location]);
+
   return (
     <>
       <Header />
